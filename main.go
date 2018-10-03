a040af954f19efdb66fd14992ea1b16beee2d81a
ilya-korotya <officalbatya@gmail.com>
2018-10-03 18:19:38 +0300

    Implement example routers

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received Request: ", r.URL.Path)
+	w.Write([]byte("Hello, word!"))
+}
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("ESS_ATOMIC_SWAP_PORT")
+	if port == "" {
+		log.Fatal("ESS_ATOMIC_SWAP_PORT environment variable was not set")
+	}
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("Could not listen: ", err)
+	}
 }
