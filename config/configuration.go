b075df49cba35007b4dc02b3c8dfcd95400b98be
Krapiy <rapidograph27@gmail.com>
2018-10-05 17:21:38 +0300

    Parse env variables

--- config/configuration.go
+++ config/configuration.go
@@ -1,11 +1,18 @@
 package config
 
 import (
+	"github.com/caarlos0/env"
 	"github.com/essentiaone/ess-atomic-swap/server"
-	// "github.com/spf13/viper"
 )
 
 //Configuration is a struct that contains server struct
 type Configuration struct {
 	Server server.Settings
+}
+
+//Init - func that read env va-les
+func Init() (Configuration, error) {
+	conf := Configuration{}
+	env.Parse(&conf)
+	return conf, nil
 }
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
-	// "github.com/essentiaone/ess-atomic-swap/config"
+
+	"github.com/essentiaone/ess-atomic-swap/config"
+	"github.com/essentiaone/ess-atomic-swap/server"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -13,14 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 
-	http.HandleFunc("/", handler)
-
-	port := os.Getenv("ESS_ATOMIC_SWAP_APP_PORT")
-	if port == "" {
-		log.Fatal("ESS_ATOMIC_SWAP_APP_PORT environment variable was not set")
-	}
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal("Could not listen: ", err)
-	}
+	settings, _ := config.Init()
+	server.Init(settings.Server)
+	log.Fatal(http.ListenAndServe(":"+settings.Server.Port, nil))
 }
