bbe0ae52549f3df4705e17498f9d6d52c9e13e87
ilya-korotya <officalbatya@gmail.com>
2018-10-09 14:50:17 +0300

    Test heroku

--- server/server.go
+++ server/server.go
@@ -7,7 +7,7 @@ import (
 //Settings struct is a simple settings for the server
 type Settings struct {
 	SSL  bool
-	Port string `env:"ESS_ATOMIC_SWAP_APP_PORT"`
+	Port string `env:"PORT"`
 }
 
 func initRouting() {
